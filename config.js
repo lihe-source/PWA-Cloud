354ba95bcb2f136294bd25f0bb89879f014dcbb8
lihe-source <lihedalihome@gmail.com>
2026-07-15 09:03:23 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ window.DRIVEDOCK_CONFIG = Object.freeze({
   DRIVE_FOLDER_NAME: "",
   MAX_FILE_BYTES: 524288000,
   UPLOAD_CHUNK_BYTES: 8388608,
-  VERSION: "2.7.0",
+  VERSION: "2.8.0",
   BUILD_DATE: "2026-07-14",
-  CACHE_NAME: "drivedock-v2.7.0",
+  CACHE_NAME: "drivedock-v2.8.0",
 });
